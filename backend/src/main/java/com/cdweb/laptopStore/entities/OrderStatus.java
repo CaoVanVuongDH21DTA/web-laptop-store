469c7ab8fe524d60a714a0370d6107ba2ba97f19
Tmthang1601 <minhthangtran1601@gmail.com>
2025-06-07 21:42:44 +0700

    khoi tao spring boot server

--- backend/src/main/java/com/cdweb/laptopStore/entities/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.cdweb.laptopStore.entities;
-
-public enum OrderStatus {
-    PENDING,
-    IN_PROGRESS,
-    SHIPPED,
-    DELIVERED,
-    CANCELLED
-}
--- backend/src/main/java/com/cdweb/laptopStore/entities/PaymentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.cdweb.laptopStore.entities;
-
-public enum PaymentStatus {
-    PENDING,
-    COMPLETED,
-    FAILED
-}
