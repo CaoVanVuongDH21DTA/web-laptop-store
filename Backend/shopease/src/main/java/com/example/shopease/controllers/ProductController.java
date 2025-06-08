ef6c0992aa7347d4f1e393a84501a3f3d7e76b16
Tmthang1601 <minhthangtran1601@gmail.com>
2025-06-08 15:08:26 +0700

    create productdto controller

---
+++ Backend/shopease/src/main/java/com/example/shopease/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package com.example.shopease.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+}
